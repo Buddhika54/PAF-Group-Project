3d2cab894a8e24330e4c7698e349599f6cba14e5
Buddhika54 <buddhikaeranga54@gmail.com>
2026-04-04 16:50:04 +0530

    Create OAuth2 Login part and create Login.jsx as well

--- smart-campus-client/src/api/axiosInstance.js
+++ smart-campus-client/src/api/axiosInstance.js
@@ -5,13 +5,31 @@ const api = axios.create({
   withCredentials: true,
 });
 
-// Attach token to every request automatically
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if(token){
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.clear();
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
   }
-  return config;
-});
+);
+
+//_____Booking_____
+export const bookingAPI = {
+    create: (data) => api.post('/api/bookings', data),
+}
 
 export default api;
