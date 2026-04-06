592e78040fb5af23bfc691d57e5ba01428c766d2
Theekshana <Hello@LAPTOP-O3U0VFFO>
2026-04-06 22:46:13 +0530

    Install jwt-decode and fix import

--- smart-campus-client/src/App.jsx
+++ smart-campus-client/src/App.jsx
@@ -1,18 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
 import Resources from './pages/resource/Resources';
 import ResourceDetails from './pages/resource/ResourceDetails';
 import EditResource from './pages/resource/EditResource';
 import AddResource from './pages/resource/AddResource'; 
-=======
 import { AuthProvider } from './context/AuthContext';
-
 import Login from './pages/Login';
 import AuthCallback from './pages/AuthCallback';
->>>>>>> dev
 
-// Each member fills in their own pages
-// Leader creates placeholder pages for now
 
 function App() {
   return (
@@ -24,18 +18,12 @@ function App() {
           <Route path="/auth/callback" element={<AuthCallback />} />
       
 
-<<<<<<< HEAD
+
         {/* Member 1 */}
         <Route path="/admin/resources" element={<Resources />} />
         <Route path="/admin/resources/add" element={<AddResource />} />
         <Route path="/admin/resources/edit/:id" element={<EditResource />} />
         <Route path="/admin/resources/:id" element={<ResourceDetails />} />
-=======
-          {/* Member 1 */}
-          <Route path="/resources" element={<div>Resource List</div>} />
-          <Route path="/resources/:id" element={<div>Resource Detail</div>} />
->>>>>>> dev
-
           {/* Member 2 */}
           <Route path="/bookings" element={<div>Bookings</div>} />
 
